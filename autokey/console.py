c25ecd4ffcf28f10234dc83cdeb9d90e05405798
Torsten Martinsen <torsten@bullestock.net>
2022-10-09 21:19:28 +0200

    Check for running process.

--- autokey/console.py
+++ autokey/console.py
@@ -1,4 +1,18 @@
-if window.wait_for_exist('terminator', timeOut=0):
-    window.activate('terminator', False, False)
+import psutil
+
+def is_process_running(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+if is_process_running('terminator'):
+    window.activate('terminator', matchClass=True)
 else:
     system.exec_command('terminator', getOutput=False)
